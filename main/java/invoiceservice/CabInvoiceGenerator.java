01fc7bdf0dc65f1bf3afc3bbbe626e82d0d5e1f8
gautamb97 <www.gautam971997@gmail.com>
2021-03-12 15:51:50 +0530

    Added:Ability to find the minimum fare

--- main/java/invoiceservice/CabInvoiceGenerator.java
+++ main/java/invoiceservice/CabInvoiceGenerator.java
@@ -3,8 +3,12 @@ package invoiceservice;
 public class CabInvoiceGenerator {
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final double MINIMUM_FARE = 5;
 
-    public double calculateFair(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if(totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
