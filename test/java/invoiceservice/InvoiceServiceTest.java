16c23e2ed13109781f63a2e344dfaf54f64d96bb
gautamb97 <www.gautam971997@gmail.com>
2021-03-12 15:48:42 +0530

    Added:Testing for minimum fare which is rs.5

--- test/java/invoiceservice/InvoiceServiceTest.java
+++ test/java/invoiceservice/InvoiceServiceTest.java
@@ -6,11 +6,20 @@ import org.junit.Test;
 
 public class InvoiceServiceTest {
     @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFair(){
+    public void givenDistanceAndTime_ShouldReturnTotalFare(){
         CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double distance = 2.0;
         int time = 5;
-        double fare = cabInvoiceGenerator.calculateFair(distance, time);
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25,fare,0.0);
+    }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare(){
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
     }
 }
